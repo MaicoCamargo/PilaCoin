d3ae2c480ac5eafd986dfb558957752c8496a7c0
MaicoCamargo <Portella08>
2018-04-25 14:21:43 -0300

    configurado aplicação do para teste locais

--- src/br/ufsm/csi/seguranca/Main.java
+++ src/br/ufsm/csi/seguranca/Main.java
@@ -16,7 +16,7 @@ public class Main {
         enviarDataGrama();
         PilaCoin pilaCoin = new PilaCoin();
         pilaCoin.setChaveCriador(RSAUtil.getPublicKey("public_key.der"));
-        pilaCoin.setIdCriador("MCamargo");
+        pilaCoin.setIdCriador("M. Camargo");
         pilaCoin.setDataCriacao(new Date());
     }
 
@@ -49,38 +49,39 @@ public class Main {
     public static void enviarDataGrama() throws Exception {
         final String meuId="Maico C.";
         final int porta= 3333;
+        //msg envia pro servidor
         Mensagem mensagem = new Mensagem();
         mensagem.setIdOrigem(meuId);
         mensagem.setTipo(Mensagem.TipoMensagem.DISCOVER);
-        mensagem.setChavePublica(RSAUtil.getPublicKey("public_key.der"));
-        mensagem.setPorta(44447);
+        mensagem.setChavePublica(RSAUtil.getPublicKey("public_key.der")); // minha chave publica
+        mensagem.setPorta(4444); // porta onde eu recebo a resposta
 
-        //socket cliente
-        DatagramSocket clientSocket = new DatagramSocket(porta);
+        //socket cliente - onde são enviados os dados
+        DatagramSocket clientSocket = new DatagramSocket(4444);
         String  servidor = "127.0.0.1";
-
         InetAddress IPAddress = InetAddress.getByName(servidor);
         System.out.println("   -- mensagem criada  para mandar para o datagrama -> \n   ");
-        byte[] mensagemSerealizada = serealizarObjeto(mensagem);
+        byte[] mensagemSerealizada = serealizarObjeto(mensagem);// serealiza a msg
 
         //envia pacote UDP para servidor
         DatagramPacket sendPacket = new DatagramPacket(mensagemSerealizada,mensagemSerealizada.length, IPAddress, porta);
         System.out.println("Enviando pacote UDP para: " + servidor + ":" + porta);
         clientSocket.send(sendPacket);
 
-        byte[] response = new byte[1500];
+        byte[] response = new byte[1500];//resposta que recebo do servidor UDP
         while (true){
 
             //recebe de volta pacote UDP do servidor
             DatagramPacket receivePacket = new DatagramPacket(response, response.length);
             clientSocket.receive(receivePacket);
-            Mensagem resposta = (Mensagem) deserializarObjeto(receivePacket.getData());
-            //System.out.println("Resposta: "+mensagem.getIdOrigem() + "ip: "+mensagem.getEndereco() + "Porta: "+mensagem.getPorta());
-
-            if (resposta.getTipo().equals(Mensagem.TipoMensagem.DISCOVER)){
+            Mensagem respostaServidor = (Mensagem) deserializarObjeto(receivePacket.getData());//desserealizo a msg recebida
+            if (respostaServidor.getTipo() == Mensagem.TipoMensagem.DISCOVER){
                 System.out.println("minha mensagem !");
             }else{
                 System.out.println("não é minha mensagem *-*");
+                System.out.println("- "+respostaServidor.getIdOrigem() + " ip: "+respostaServidor.getEndereco() + " Porta: "+respostaServidor.getPorta());
+                System.out.println("- MASTER: "+respostaServidor.isMaster());
+                System.out.println("- TIPO: "+respostaServidor.getTipo());
             }
             System.out.println("final do socket !");
         }
